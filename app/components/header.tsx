931bf74e2d856c9e5d5e686ea955a167ce23d0fa
GAKUNEMOTO <nemotogaku1@gmail.com>
2024-04-10 21:56:57 -0500

    delete(search.tsx) delete search.tsx update(favicon):add new favicon

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import MobileNav from './mobile-nav'
-import Search from './search'
 import { AppConfig } from '@/app.config'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { Pickaxe, PocketKnife } from 'lucide-react'
 
 export default function Header() {
   return (
     <header className='border-b'>
-        <div className="container  h-16 flex items-center gap-4">
+        <div className="container h-16 flex items-center gap-4">
             <div className="lg:hidden">
                 <MobileNav/>
             </div>
-            <p className='font-bold text-lg'>{AppConfig.title}</p>
-            <Search/>
+            <Button asChild variant='ghost' className='font-bold text-lg gap-2'>
+              <Link href='/' replace>
+                <Pickaxe size={18}/>           
+              {AppConfig.title}
+              </Link>
+              </Button>
         </div>
     </header>
   )
